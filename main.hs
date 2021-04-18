c5c6fd436025cb11cfb44dd0ecf61553a6d0fa18
lucasgsa <lucasgsalmeida19@gmail.com>
2021-04-18 03:20:57 -0300

    adicionando mais testes

--- main.hs
+++ main.hs
@@ -8,6 +8,7 @@ main = do
     let newJogo3 = Jogo.Jogo "LoL" ["moba", "riot", "competitivo", "jogo em equipe", "habilidades"] "pentium" [8, 8.5, 9, 10, 8.9]
     let newJogo4 = Jogo.Jogo "Valorant" ["fps", "riot", "competitivo", "jogo em equipe", "habilidades"] "i3" [8, 8.5, 9, 10, 8.9]
     let listaJogos = [newJogo, newJogo2, newJogo3, newJogo4]
+    
     -- Lista todos os jogos
     putStrLn ("Todos os jogos:")
     putStrLn (show (Jogo.nomeJogos listaJogos))
@@ -27,3 +28,17 @@ main = do
     -- Jogos em equipe
     putStrLn ("Jogos moba:")
     putStrLn (show (Jogo.nomeJogos (Jogo.jogosCategoria "moba" listaJogos)))
+
+    -- Medias de avaliação
+    putStrLn ("Médias de avaliação:")
+    putStrLn ("CS = " ++ show (Jogo.mediaAvaliacoesPorNome "CS" listaJogos))
+    putStrLn ("LoL = " ++ show (Jogo.mediaAvaliacoesPorNome "LoL" listaJogos))
+    putStrLn ("Dota = " ++ show (Jogo.mediaAvaliacoesPorNome "Dota" listaJogos))
+    putStrLn ("Valorant = " ++ show (Jogo.mediaAvaliacoesPorNome "Valorant" listaJogos))
+
+    -- Requisitos Mínimos
+    putStrLn ("Requisitos Mínimos:")
+    putStrLn ("CS = " ++ (Jogo.reqMinimosPorNome "CS" listaJogos))
+    putStrLn ("LoL = " ++ (Jogo.reqMinimosPorNome "LoL" listaJogos))
+    putStrLn ("Dota = " ++ (Jogo.reqMinimosPorNome "Dota" listaJogos))
+    putStrLn ("Valorant = " ++ (Jogo.reqMinimosPorNome "Valorant" listaJogos))
