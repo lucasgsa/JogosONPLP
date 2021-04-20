11f113fa9a6aad19d14e3c4762fed7f2f1ebb99e
lucasgsa <lucasgsalmeida19@gmail.com>
2021-04-20 03:02:14 -0300

    adicionando opção de letra em negrito e de limpar cor

--- src/Util.hs
+++ src/Util.hs
@@ -5,13 +5,16 @@ module Util where
     listToString (x:[]) = x 
     listToString (x:xs) =  x ++ ", " ++ listToString xs
 
-    color :: String -> String
-    color "nocolor" = "\ESC[m"
-    color "black"   = "\ESC[30m"
-    color "red"     = "\ESC[31m"
-    color "green"   = "\ESC[32m"
-    color "yellow"  = "\ESC[33m"
-    color "blue"    = "\ESC[34m"
-    color "magenta" = "\ESC[35m"
-    color "cyan"    = "\ESC[36m"
-    color "white"   = "\ESC[37m"
+    color :: String -> Bool -> String
+    color "nocolor" _    = "\ESC[0m"
+    color "black"   bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[30m"
+    color "red"     bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[31m" 
+    color "green"   bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[32m"
+    color "yellow"  bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[33m"
+    color "blue"    bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[34m"
+    color "magenta" bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[35m"
+    color "cyan"    bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[36m"
+    color "white"   bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[37m"
+
+    bold = "\ESC[1m"
+    nobold = "\ESC[0m"
