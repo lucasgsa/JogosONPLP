ca59a874e920d9214e918aea42b46483e69cd39a
lucasgsa <lucasgsalmeida19@gmail.com>
2021-04-20 02:47:55 -0300

    adicionando funcionalidades 3 e 5

---
+++ src/Listagem.hs
@@ -0,0 +1,24 @@
+module Listagem where
+    import Util as Util
+    import Jogo as Jogo
+
+    -- 3. Deve ser possível listar todos os jogos disponíveis
+    listarJogos :: [Jogo.Jogo] -> String
+    listarJogos [] = "Nenhum jogo encontrado."
+    listarJogos x = (Util.color "blue") ++ "Todos os jogos: " ++ "\n" ++ listarJogosAux x
+
+    listarJogosAux :: [Jogo.Jogo] -> String 
+    listarJogosAux [] = ""
+    listarJogosAux (x:[]) = show x
+    listarJogosAux (x:xs) = (show x) ++ "\n\n" ++ listarJogosAux xs
+
+    -- 5. Deve ser possível listar os jogos por categoria;
+    listarJogosCategoria :: String -> [Jogo.Jogo] -> String
+    listarJogosCategoria categoria listaJogos = (Util.color "blue") ++ "Jogos da categoria "++categoria++": "++ "\n"
+                                                ++listarJogosAux (filterCategoria categoria listaJogos)
+
+    filterCategoria :: String -> [Jogo.Jogo] -> [Jogo.Jogo]
+    filterCategoria categoria listaJogos = [ x | x <- listaJogos, (categoriaContem categoria x)]
+
+    categoriaContem :: String -> Jogo -> Bool
+    categoriaContem categoria jogo = categoria `elem` (Jogo.categorias jogo)
