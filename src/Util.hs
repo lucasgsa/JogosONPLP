63b173c5b3228d59a757ffd165e8cff75e64daf0
lucasgsa <lucasgsalmeida19@gmail.com>
2021-04-20 14:36:16 -0300

    agora a função cor recebe a string para ser colorida e automaticamente da clear na cor depois

--- src/Util.hs
+++ src/Util.hs
@@ -5,16 +5,16 @@ module Util where
     listToString (x:[]) = x 
     listToString (x:xs) =  x ++ ", " ++ listToString xs
 
-    color :: String -> Bool -> String
-    color "nocolor" _    = "\ESC[0m"
-    color "black"   bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[30m"
-    color "red"     bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[31m" 
-    color "green"   bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[32m"
-    color "yellow"  bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[33m"
-    color "blue"    bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[34m"
-    color "magenta" bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[35m"
-    color "cyan"    bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[36m"
-    color "white"   bold = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[37m"
+    color :: String -> Bool -> String -> String
+    color "nocolor" _ _   = "\ESC[0m"
+    color "black"   bold texto = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[30m" ++ texto ++ "\ESC[0m"
+    color "red"     bold texto = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[31m" ++ texto ++ "\ESC[0m"
+    color "green"   bold texto = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[32m" ++ texto ++ "\ESC[0m"
+    color "yellow"  bold texto = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[33m" ++ texto ++ "\ESC[0m"
+    color "blue"    bold texto = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[34m" ++ texto ++ "\ESC[0m"
+    color "magenta" bold texto = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[35m" ++ texto ++ "\ESC[0m"
+    color "cyan"    bold texto = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[36m" ++ texto ++ "\ESC[0m"
+    color "white"   bold texto = (if bold then "\ESC[1m" else "\ESC[0m") ++ "\ESC[37m" ++ texto ++ "\ESC[0m"
 
     bold = "\ESC[1m"
     nobold = "\ESC[0m"
