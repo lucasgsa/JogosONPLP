d5594284b2facb2f92c09ce0ca0505d5e23d5b88
lucasgsa <lucasgsalmeida19@gmail.com>
2021-04-20 14:35:27 -0300

    agora a função de cor recebe a string a ser colorida

--- src/Jogo.hs
+++ src/Jogo.hs
@@ -11,12 +11,12 @@ data Jogo = Jogo {
 } deriving (Eq,Ord)
 
 instance Show Jogo where
-  show (Jogo nome categorias reqMinimos plataforma preco online) = (Util.color "green" True) ++ "  Título: " ++ (Util.color "cyan" False) ++ nome ++ "\n"
-                                                                   ++ (Util.color "white" False) ++ "   Plataforma: " ++ plataforma ++ "\n"
-                                                                   ++ (Util.color "white" False) ++ "   Requisitos Mínimos: " ++ (Util.listToString reqMinimos) ++ "\n"
-                                                                   ++ (Util.color "white" False) ++ "   Gêneros: " ++ (Util.listToString categorias) ++ "\n"
-                                                                   ++ (Util.color "yellow" False) ++ "   " ++ (if online then "Cooperativo online" else "Um jogador") ++ "\n"
-                                                                   ++ (Util.color "red" True) ++ "   Preço: " ++ (if (preco /= 0) then ("R$" ++ show preco) else "Grátis") ++ (Util.color "nocolor" False)
+  show (Jogo nome categorias reqMinimos plataforma preco online) = (Util.color "green" True "  Título: ") ++ (Util.color "cyan" False nome) ++ "\n"
+                                                                   ++ (Util.color "white" False ("   Plataforma: " ++ plataforma)) ++ "\n"
+                                                                   ++ (Util.color "white" False ("   Requisitos Mínimos: " ++ (Util.listToString reqMinimos))) ++ "\n"
+                                                                   ++ (Util.color "white" False ("   Gêneros: " ++ (Util.listToString categorias))) ++ "\n"
+                                                                   ++ (Util.color "yellow" False ("   " ++ (if online then "Cooperativo online" else "Um jogador"))) ++ "\n"
+                                                                   ++ (Util.color "red" True ("   Preço: " ++ (if (preco /= 0) then ("R$" ++ show preco) else "Grátis")))
 convertArrayToString :: [String] -> String
 convertArrayToString (h: []) = h
 convertArrayToString (h:t) = h ++ "," ++ convertArrayToString (t)
