69432d8a56145bb9994040d98686a2f902c00268
lucasgsa <lucasgsalmeida19@gmail.com>
2021-04-20 02:47:29 -0300

    criando função que retorna uma cor para ser usada na saída

--- src/Util.hs
+++ src/Util.hs
@@ -4,3 +4,14 @@ module Util where
     listToString [] = ""
     listToString (x:[]) = x 
     listToString (x:xs) =  x ++ ", " ++ listToString xs
+
+    color :: String -> String
+    color "nocolor" = "\ESC[m"
+    color "black"   = "\ESC[30m"
+    color "red"     = "\ESC[31m"
+    color "green"   = "\ESC[32m"
+    color "yellow"  = "\ESC[33m"
+    color "blue"    = "\ESC[34m"
+    color "magenta" = "\ESC[35m"
+    color "cyan"    = "\ESC[36m"
+    color "white"   = "\ESC[37m"
