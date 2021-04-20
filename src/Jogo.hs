7b89678cd8201868b4f3a8375a094cc20767b19c
lucasgsa <lucasgsalmeida19@gmail.com>
2021-04-20 02:47:05 -0300

    adicionando cores ao show de Jogo

--- src/Jogo.hs
+++ src/Jogo.hs
@@ -11,12 +11,12 @@ data Jogo = Jogo {
 } deriving (Eq,Ord)
 
 instance Show Jogo where
-  show (Jogo nome categorias reqMinimos plataforma preco online) = "Título: " ++ nome ++ "\n"
-                                                                   ++ " Plataforma: " ++ plataforma ++ "\n"
-                                                                   ++ " " ++ (if online then "Cooperativo online" else "Um jogador") ++ "\n"
-                                                                   ++ " Requisitos Mínimos: " ++ (Util.listToString reqMinimos) ++ "\n"
-                                                                   ++ " Gêneros: " ++ (Util.listToString categorias) ++ "\n"
-                                                                   ++ " Preço: " ++ (if (preco /= 0) then ("R$" ++ show preco) else "Grátis")
+  show (Jogo nome categorias reqMinimos plataforma preco online) = (Util.color "green") ++ "  Título: \x1b[30m" ++ (Util.color "red") ++ nome ++ "\n"
+                                                                   ++ (Util.color "yellow") ++ "   Plataforma: " ++ plataforma ++ "\n"
+                                                                   ++ (Util.color "yellow") ++ "   " ++ (if online then "Cooperativo online" else "Um jogador") ++ "\n"
+                                                                   ++ (Util.color "yellow") ++ "   Requisitos Mínimos: " ++ (Util.listToString reqMinimos) ++ "\n"
+                                                                   ++ (Util.color "yellow") ++ "   Gêneros: " ++ (Util.listToString categorias) ++ "\n"
+                                                                   ++ (Util.color "yellow") ++ "   Preço: " ++ (if (preco /= 0) then ("R$" ++ show preco) else "Grátis") ++ (Util.color "nocolor")
 convertArrayToString :: [String] -> String
 convertArrayToString (h: []) = h
 convertArrayToString (h:t) = h ++ "," ++ convertArrayToString (t)
