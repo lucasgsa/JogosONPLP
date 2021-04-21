98b8b82a6980bc2f5ad0c3d76b28e06715c31b8d
hiarlyfs <hiarlyfsouto@gmail.com>
2021-04-20 23:48:12 -0300

    Add ano lançamento no jogo

--- src/Jogo.hs
+++ src/Jogo.hs
@@ -7,14 +7,16 @@ data Jogo = Jogo {
   reqMinimos :: [String],
   plataforma :: String,
   preco :: Double,
-  online :: Bool
+  online :: Bool,
+  anoLancamento :: Integer
 } deriving (Eq,Ord)
 
 instance Show Jogo where
-  show (Jogo nome categorias reqMinimos plataforma preco online) = (Util.color "green" True "  Título: ") ++ (Util.color "cyan" False nome) ++ "\n"
+  show (Jogo nome categorias reqMinimos plataforma preco online anoLancamento) = (Util.color "green" True "  Título: ") ++ (Util.color "cyan" False nome) ++ "\n"
                                                                    ++ (Util.color "white" False ("   Plataforma: " ++ plataforma)) ++ "\n"
                                                                    ++ (Util.color "white" False ("   Requisitos Mínimos: " ++ (Util.listToString reqMinimos))) ++ "\n"
                                                                    ++ (Util.color "white" False ("   Gêneros: " ++ (Util.listToString categorias))) ++ "\n"
+                                                                   ++ (Util.color "white" False ("   Ano lançamento: " ++ show anoLancamento)) ++ "\n"
                                                                    ++ (Util.color "yellow" False ("   " ++ (if online then "Cooperativo online" else "Um jogador"))) ++ "\n"
                                                                    ++ (Util.color "red" True ("   Preço: " ++ (if (preco /= 0) then ("R$" ++ show preco) else "Grátis")))
 convertArrayToString :: [String] -> String
@@ -26,6 +28,6 @@ getArrayToString array = "[" ++ convertArrayToString(array) ++ "]"
 
 salvarJogo :: Jogo.Jogo -> IO()
 salvarJogo jogo = do
-  let jogoStr = Jogo.nome jogo ++ "," ++ getArrayToString (Jogo.categorias jogo) ++ "," ++ getArrayToString (Jogo.reqMinimos jogo) ++ "," ++ Jogo.plataforma jogo ++ "," ++ show (Jogo.preco jogo) ++ "," ++ show (Jogo.online jogo)
+  let jogoStr = Jogo.nome jogo ++ "," ++ getArrayToString (Jogo.categorias jogo) ++ "," ++ getArrayToString (Jogo.reqMinimos jogo) ++ "," ++ Jogo.plataforma jogo ++ "," ++ show (Jogo.preco jogo) ++ "," ++ show (Jogo.online jogo) ++ "," ++ show (Jogo.anoLancamento jogo)
   appendFile "dados/jogos.csv" (jogoStr ++ "\n")
   return ()
