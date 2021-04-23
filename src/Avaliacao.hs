3c5fd797d7757239f1d5d75cfc111db5a86d0a22
lucasgsa <lucasgsalmeida19@gmail.com>
2021-04-23 01:10:58 -0300

    adicionando instancia de Show

--- src/Avaliacao.hs
+++ src/Avaliacao.hs
@@ -7,10 +7,12 @@ data Avaliacao = Avaliacao {
   comentario :: String
 } deriving (Eq,Ord)
 
+instance Show Avaliacao where
+  show (Avaliacao usuario jogo nota comentario) = usuario ++ " "++ jogo ++ " " ++  (show nota) ++ " " ++ comentario
+
 salvarAvaliacao :: Avaliacao.Avaliacao -> IO()
 salvarAvaliacao avaliacao =  do
   -- TODO: Verificar se o jogo e o usuário estão cadastrados (Case Insensitive)
   let avaliacaoStr = Avaliacao.usuario avaliacao ++ "|" ++ Avaliacao.jogo avaliacao ++ "|" ++  show (Avaliacao.nota avaliacao) ++ "|" ++  Avaliacao.comentario avaliacao
-  appendFile "dados/avaliacao.txt" (avaliacaoStr ++ "\n")
+  appendFile "dados/avaliacoes.txt" (avaliacaoStr ++ "\n")
   return ()
-
