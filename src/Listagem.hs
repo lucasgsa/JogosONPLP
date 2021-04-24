b80ee8a3a4dca82e622606cb9a666a4ae55ebb75
Renan Carneiro <renan.araujo@ccc.ufcg.edu.br>
2021-04-23 22:42:51 -0300

    Fix listagem por avaliação e excesso de parênteses

--- src/Listagem.hs
+++ src/Listagem.hs
@@ -1,13 +1,13 @@
 module Listagem where
-    import Util as Util
-    import Jogo as Jogo
-    import Avaliacao as Avaliacao
+    import Util
+    import Jogo
+    import Avaliacao
 
     -- 3. Deve ser possível listar todos os jogos disponíveis
     listarJogos :: [Jogo.Jogo] -> String
-    listarJogos x = (Util.color "blue" True ("Todos os jogos: ")) ++ "\n" ++ listarJogosAux x
+    listarJogos x = Util.color "blue" True "Todos os jogos: " ++ "\n" ++ listarJogosAux x
 
-    listarJogosAux :: [Jogo.Jogo] -> String 
+    listarJogosAux :: [Jogo.Jogo] -> String
     listarJogosAux [] = "Nenhum jogo encontrado."
     listarJogosAux (x:[]) = show x
     listarJogosAux (x:xs) = (show x) ++ "\n\n" ++ listarJogosAux xs
@@ -18,19 +18,19 @@ module Listagem where
 
     -- 5. Deve ser possível listar os jogos por categoria;
     listarJogosCategoria :: [Char] -> [Jogo] -> [Char]
-    listarJogosCategoria categoria listaJogos = (Util.color "blue" True ("Jogos da categoria "++categoria++": ")) ++ "\n"
+    listarJogosCategoria categoria listaJogos = Util.color "blue" True ("Jogos da categoria "++categoria++": ") ++ "\n"
                                                 ++listarJogosAux (filterCategoria categoria listaJogos)
 
     filterCategoria :: String -> [Jogo.Jogo] -> [Jogo.Jogo]
-    filterCategoria categoria listaJogos = [ x | x <- listaJogos, (categoriaContem categoria x)]
+    filterCategoria categoria listaJogos = [ x | x <- listaJogos, categoriaContem categoria x]
 
     categoriaContem :: String -> Jogo -> Bool
-    categoriaContem categoria jogo = categoria `elem` (Jogo.categorias jogo)
+    categoriaContem categoria jogo = categoria `elem` Jogo.categorias jogo
 
     -- 6. Deve ser possível listar os jogos em ordem de lançamento.
 
     listarJogosPorAnoLancamento :: [Jogo.Jogo ] -> String
-    listarJogosPorAnoLancamento listarJogos = listarJogosAux(take 10 (reverse((mergeSort listarJogos))))
+    listarJogosPorAnoLancamento listarJogos = listarJogosAux(take 10 (reverse(mergeSort listarJogos)))
 
     -- tipo 1 - normal, verificando x <= y
     -- tipo 2 - por ano de lançamento
@@ -47,7 +47,7 @@ module Listagem where
     merge xs [] = xs
     merge [] ys = ys
     merge (x:xs) (y:ys)
-        | (Jogo.anoLancamento x <= Jogo.anoLancamento y) = x:merge xs (y:ys)
+        | Jogo.anoLancamento x <= Jogo.anoLancamento y = x:merge xs (y:ys)
         | otherwise = y:merge (x:xs) ys
 
     primeiraParte :: [Jogo] -> [Jogo]
@@ -59,22 +59,22 @@ module Listagem where
     -- 9 - Deve ser possível listar as avaliações de um jogo. 
     listarAvaliacoesJogo :: String -> [Jogo] -> [Avaliacao] -> String
     listarAvaliacoesJogo _ _ []= "Não há avaliações no sistema."
-    listarAvaliacoesJogo nomeJogo jogos avaliacoes = if (existeJogo nomeJogo jogos)
-                                                    then (Util.color "red" True ("Avaliações do jogo "++nomeJogo ++ ":\n")) 
-                                                        ++ (Util.color "green" False "Nota média do jogo: " ++ (show (getMediaJogo nomeJogo avaliacoes)) ++ "\n")
+    listarAvaliacoesJogo nomeJogo jogos avaliacoes
+        |existeJogo nomeJogo jogos = Util.color "red" True ("Avaliações do jogo "++Util.color "white" True nomeJogo ++ ":\n")
+                                                        ++ (Util.color "green" False "Nota média do jogo: " ++ show (getMediaJogo nomeJogo avaliacoes) ++ "\n")
                                                         ++ listarAvaliacoesJogoAux nomeJogo avaliacoes
-                                                    else (Util.color "red" True ("O jogo "++ nomeJogo ++ " não está cadastrado."))
+        |listarAvaliacoesJogoAux nomeJogo avaliacoes == "" = Util.color "red" True ("O jogo "++ nomeJogo ++ " não possui avaliações no sistema.")
+        |otherwise = Util.color "red" True ("O jogo "++ nomeJogo ++ " não está cadastrado.")
+
 
     existeJogo :: String -> [Jogo] -> Bool
     existeJogo _ [] = False
-    existeJogo nomeJogo (x:xs) = if (Jogo.nome x) == nomeJogo
-                                then True
-                                else existeJogo nomeJogo xs
+    existeJogo nomeJogo (x:xs) = (Jogo.nome x == nomeJogo) || existeJogo nomeJogo xs
 
     listarAvaliacoesJogoAux :: String -> [Avaliacao] -> String
     listarAvaliacoesJogoAux _ [] = ""
     listarAvaliacoesJogoAux nomeJogo (x:xs) = if nomeJogo == (Avaliacao.jogo x)
-                                            then show x ++ "\n\n" ++ (listarAvaliacoesJogoAux nomeJogo xs)
+                                            then show x ++ "\n\n" ++ listarAvaliacoesJogoAux nomeJogo xs
                                             else listarAvaliacoesJogoAux nomeJogo xs
 
     -- Retorna a média de um jogo.
@@ -82,23 +82,23 @@ module Listagem where
     getMediaJogo _ [] = 0.0
     getMediaJogo nomeJogo avaliacoes = do
                                     let aux = getAvaliacoesJogo nomeJogo avaliacoes
-                                    if ((snd aux) == 0) then 0.0 else ((fst aux)/ (fromIntegral (snd aux)))
+                                    if snd aux == 0 then 0.0 else fst aux/ fromIntegral (snd aux)
 
     -- Retorna uma tupla, com o primeiro elemento contendo a soma das notas, e o segundo a quantidade de avaliacoes.
     -- Ou seja, a media é so fazer fst/snd.
     getAvaliacoesJogo :: String -> [Avaliacao] -> (Double, Integer)
     getAvaliacoesJogo _ [] = (0.0, 0)
     getAvaliacoesJogo nomeJogo (x:xs) = do
-                                    let atual = (if (nomeJogo) == (Avaliacao.jogo x) then ((Avaliacao.nota x), 1) else (0,0))
-                                    ((fst atual) + (fst prox), (snd atual) + (snd prox))
+                                    let atual = (if nomeJogo == Avaliacao.jogo x then (Avaliacao.nota x, 1) else (0,0))
+                                    (fst atual + fst prox, snd atual + snd prox)
                                     where prox = getAvaliacoesJogo nomeJogo xs
 
     ordenaAvaliacoes :: [Jogo.Jogo] -> [Avaliacao] -> [Jogo.Jogo]
     ordenaAvaliacoes [] _ = []
-    ordenaAvaliacoes (s:xs) z = ordenaAvaliacoes [x|x <- xs, (getMediaJogo (Jogo.nome x) z) < (getMediaJogo (Jogo.nome s) z)] z
+    ordenaAvaliacoes (s:xs) z = ordenaAvaliacoes [x|x <- xs, getMediaJogo (Jogo.nome x) z < getMediaJogo (Jogo.nome s) z] z
         ++ [s] ++
-     ordenaAvaliacoes [x|x <- xs,(getMediaJogo (Jogo.nome x) z) >= (getMediaJogo (Jogo.nome s) z)] z    
-     
+     ordenaAvaliacoes [x|x <- xs,getMediaJogo (Jogo.nome x) z >= getMediaJogo (Jogo.nome s) z] z
+
     -- 7. Deve ser possível listar os jogos com melhores avaliações;
-    listaAvaliacoesOrdenada :: [Jogo.Jogo] -> [Avaliacao]-> String   
-    listaAvaliacoesOrdenada listaJogos listaAvaliacoes = listarJogosAux(reverse (ordenaAvaliacoes listaJogos listaAvaliacoes))  
+    listaAvaliacoesOrdenada :: [Jogo.Jogo] -> [Avaliacao]-> String
+    listaAvaliacoesOrdenada listaJogos listaAvaliacoes = listarJogosAux(reverse (ordenaAvaliacoes listaJogos listaAvaliacoes))
