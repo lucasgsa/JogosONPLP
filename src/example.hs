125eb92e7e2d5178fc5fcdda5e515d78e1eba1e8
lucasgsa <lucasgsalmeida19@gmail.com>
2021-04-25 22:41:11 -0300

    adicionando mais um exemplo agora da 10º funcionalidade

--- src/example.hs
+++ src/example.hs
@@ -2,6 +2,7 @@ import Jogo
 import Listagem
 import Util
 import CarregaArquivos
+import Indicacao
 
 main = do
     jogos <- CarregaArquivos.lerArquivoJogos "dados/jogos.txt"
@@ -12,13 +13,18 @@ main = do
 
     let listaAvaliacoes = CarregaArquivos.carregarAvaliacoes avaliacoes
 
-    --putStrLn (Listagem.listarJogosPorAnoLancamento listaJogos)
+    putStrLn (Listagem.listarJogosPorAnoLancamento listaJogos)
 
-    --putStrLn (Listagem.listarAvaliacoesJogo "Counter-Strike: Global Offensive" listaJogos listaAvaliacoes)
+    putStrLn (Listagem.listarAvaliacoesJogo "Counter-Strike: Global Offensive" listaJogos listaAvaliacoes)
 
-    --putStrLn (Listagem.listarJogos listaJogos)
+    putStrLn (Listagem.listarJogos listaJogos)
 
-    --putStrLn (Listagem.listaAvaliacoesOrdenada  listaJogos listaAvaliacoes)
+    putStrLn (Listagem.listaAvaliacoesOrdenada  listaJogos listaAvaliacoes)
 
     putStrLn (Listagem.listarAvaliacoesJogo "Among Us" listaJogos listaAvaliacoes)
+
+    -- Listando 3 indicações para o usuario.
+    putStrLn (show (Indicacao.indicarJogo "Kipon" listaAvaliacoes listaJogos 0))
+    putStrLn (show (Indicacao.indicarJogo "Kipon" listaAvaliacoes listaJogos 1))
+    putStrLn (show (Indicacao.indicarJogo "Kipon" listaAvaliacoes listaJogos 2))
 
